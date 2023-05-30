001536e1a30e8e238b96373cf0f1b2eb4ca7760c
Nurcholis Al Fachri <nurcholis.alfachri@gmail.com>
2023-05-30 12:59:40 +0700

    add semua file

---
+++ script.js
@@ -0,0 +1,95 @@
+const searchButton = document.querySelector('.search-button');
+searchButton.addEventListener('click', async function() {
+    try {
+        const inputKeyword = document.querySelector('.input-keyword');
+        const movies = await getDataMovies(inputKeyword.value);
+        updateUI(movies);
+    } catch (err) {
+        console.log(err);
+        alert(err);
+    }
+});
+
+document.addEventListener('click', async function(e) {
+    if (e.target.classList.contains('modal-detail-button')) {
+        try {
+            const imdbid = e.target.dataset.imdbid;
+            const movieDataDetail = await getDataMovieDetail(imdbid);
+            updateUIDetail(movieDataDetail);
+        } catch (err) {
+            console.log(err);
+            alert(err);
+        }
+    }
+});
+
+function getDataMovies(Keyword) {
+    return fetch(`http://www.omdbapi.com/?apikey=52b597cd&s=${Keyword}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(response => {
+            if (response.Response === 'False') {
+                throw new Error(response.Error);
+            }
+            return response.Search;
+        });
+};
+
+function updateUI(movies) {
+    let cards = '';
+    movies.forEach(m => cards += showCards(m));
+    const movieContainer = document.querySelector('.movie-container');
+    movieContainer.innerHTML = cards;
+}
+
+function getDataMovieDetail(imdbid) {
+    return fetch(`http://www.omdbapi.com/?apikey=52b597cd&i=${imdbid}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(m => m);
+};
+
+function updateUIDetail(m) {
+    const movieDetail = showMovieDetail(m);
+    const modalBody = document.querySelector('.modal-body');
+    modalBody.innerHTML = movieDetail;
+}
+
+function showCards(m) {
+    return `<div class="col-md-4 my-3">
+                <div class="card">
+                    <img src="${m.Poster}" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">${m.Title}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+                        <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal" data-target="#movieDetailModal" data-imdbid="${m.imdbID}">Show Details</a>
+                    </div>
+                </div>
+            </div>`
+};
+
+function showMovieDetail(m) {
+    return `<div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-3 my-5 mx-5">
+                        <img src="${m.Poster}" class="img-fluid"></div>
+                    </div>
+                    <div class="col-md">
+                        <ul class="list-group">
+                            <li class="list-group-item"><h4>${m.Title} ${m.Year}</h4></li>
+                            <li class="list-group-item"><strong>Director: </strong>${m.Director}</li>
+                            <li class="list-group-item"><strong>Actors : </strong>${m.Actors}</li>
+                            <li class="list-group-item"><strong>Writer : </strong>${m.Writer}</li>
+                            <li class="list-group-item"><strong>Plot : </strong><br>${m.Plot}</li>
+                        </ul>
+                    </div>
+            </div>`
+};
